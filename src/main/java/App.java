073efd375c782d3635aaed67830beacae334c250
disk8362 <disk8362@mail.ru>
2023-04-15 16:58:12 +0300

    git commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,5 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +7,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat bean3 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean.getMessage());
+        Cat bean4 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean.getMessage());
+        System.out.println(bean == bean2);
+        System.out.println(bean3 == bean4);
     }
 }
